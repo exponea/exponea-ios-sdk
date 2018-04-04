48d8b3f21bf1ddb8bee5d3050ce1cb41bd643790
Ricardo Tokashiki <ricardotokashiki@Ricardos-MacBook-Pro.local>
2018-04-04 16:07:59 +0200

    Adjusting/Cleanup Database Manager

--- ExponeaSDK/ExponeaSDK/Entities/EntitiesManager.swift
+++ ExponeaSDK/ExponeaSDK/Entities/EntitiesManager.swift
@@ -13,17 +13,10 @@ import CoreData
 public protocol EntitieTrack: class {
     func trackCustumer(projectId: String, customerId: KeyValueModel, properties: [KeyValueModel])
     func trackEvents(projectId: String, customerId: KeyValueModel, properties: [KeyValueModel], timestamp: Int, eventType: String)
-}
-
-/// Protocol to manage manipulate private Tokens
-public protocol EntitieTokens: class {
-    func rotateToken(projectId: String)
-    func revokeToken(projectId: String)
-}
-
-/// Protocol to manage and modify the customer data
-public protocol EntitieCustomerData: class {
-    func anonymize(projectId: String, customerId: KeyValueModel)
+    func fetchTrackCustomer() -> [TrackCustomers]?
+    func fetchTrackEvents() -> [TrackEvents]?
+    func deleteTrackCustomer(object: AnyObject) -> Bool
+    func deleteTrackEvent(object: AnyObject) -> Bool
 }
 
 /// The Entities Manager class is responsible for persist the data using CoreData Framework.
@@ -41,6 +34,7 @@ public class EntitiesManager {
         return container
     }()
 
+    /// Save all changes in CoreData
     public func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -52,6 +46,13 @@ public class EntitiesManager {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
+    }
+
+    /// Delete a specific object in CoreData
+    private func deleteObject(_ object: NSManagedObject) {
+        let context = persistentContainer.viewContext
+        context.delete(object)
+        saveContext()
     }
 }
 
@@ -115,59 +116,68 @@ extension EntitiesManager: EntitieTrack {
         // Save the customer properties into CoreData
         saveContext()
     }
-}
 
-extension EntitiesManager: EntitieTokens {
+    /// Fetch all Tracking Customers from CoreData
+    public func fetchTrackCustomer() -> [TrackCustomers]? {
 
-    /// To rotate your private token just post an empty JSON to the following route.
-    /// The old token will still work for next 48 hours. You cannot have more than two
-    /// private tokens for one public token, therefore rotating the newly fetched token
-    /// while the old token is still working will result in revoking that old token right away.
-    /// Rotating the old token twice will result in error, since you cannot have three tokens at the same time.
-    ///
-    /// - Parameters:
-    ///     - projectId: Project token (you can find it in the Overview section of your project)
-    public func rotateToken(projectId: String) {
+        var trackCustomers = [TrackCustomers]()
 
-        let tokenRotate = TokenRotate(context: persistentContainer.viewContext)
+        do {
+            let context = persistentContainer.viewContext
+            trackCustomers = try context.fetch(TrackCustomers.fetchRequest())
+        } catch {
+            // TODO: Logging
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
 
-        tokenRotate.projectId = projectId
-        tokenRotate.timestamp = Int32(NSDate().timeIntervalSince1970)
-
-        // Save the Rotate Token into CoreData
-        saveContext()
+        return trackCustomers
     }
 
-    /// To revoke token right away just post an empty JSON to the following route.
-    /// Please note, that revoking a token can result in losing the access if you haven't revoked a new token before.
-    ///
-    /// - Parameters:
-    ///     - projectId: Project token (you can find it in the Overview section of your project)
-    public func revokeToken(projectId: String) {
+    /// Fetch all Tracking Events from CoreData
+    public func fetchTrackEvents() -> [TrackEvents]? {
 
-        let tokenRevoke = TokenRevoke(context: persistentContainer.viewContext)
+        var trackEvents = [TrackEvents]()
 
-        tokenRevoke.projectId = projectId
-        tokenRevoke.timestamp = Int32(NSDate().timeIntervalSince1970)
+        do {
+            let context = persistentContainer.viewContext
+            trackEvents = try context.fetch(TrackEvents.fetchRequest())
+        } catch {
+            // TODO: Logging
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
 
-        // Save the Revoke Token into CoreData
-        saveContext()
+        return trackEvents
     }
-}
 
-extension EntitiesManager: EntitieCustomerData {
-
-    /// Removes all the external identifiers and assigns a new cookie id. Removes all personal customer properties
+    /// Detele a Tracking Customer Object from CoreData
     ///
     /// - Parameters:
-    ///     - projectId: Project token (you can find it in the Overview section of your project)
-    ///     - customerIds: “cookie” for identifying anonymous customers or “registered” for identifying known customers)
-    public func anonymize(projectId: String, customerId: KeyValueModel) {
+    ///     - object: Tracking Customer Object to be deleted from CoreData
+    public func deleteTrackCustomer(object: AnyObject) -> Bool {
 
-        let customerAnonymize = CustomerAnonymize(context: persistentContainer.viewContext)
+        guard let trackObject = object as? TrackCustomers else {
+            // TODO: Logging
+            return false
+        }
 
-        customerAnonymize.projectId = projectId
-        customerAnonymize.customerIdKey = customerId.key
-        customerAnonymize.customerIdValue = customerId.value
+        deleteObject(trackObject)
+        return true
+    }
+
+    /// Detele a Tracking Event Object from CoreData
+    ///
+    /// - Parameters:
+    ///     - object: Tracking Event Object to be deleted from CoreData
+    public func deleteTrackEvent(object: AnyObject) -> Bool {
+
+        guard let trackEvent = object as? TrackEvents else {
+            // TODO: Logging
+            return false
+        }
+
+        deleteObject(trackEvent)
+        return true
     }
 }
