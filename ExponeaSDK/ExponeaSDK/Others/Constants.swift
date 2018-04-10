bbe5c9f6a1d3c86ace882c66a377c4b9cb90cd0d
Ricardo Tokashiki <ricardotokashiki@Ricardos-MacBook-Pro.local>
2018-04-10 12:05:28 +0200

    Cleaning project for merge to develop

--- ExponeaSDK/ExponeaSDK/Others/Constants.swift
+++ ExponeaSDK/ExponeaSDK/Others/Constants.swift
@@ -2,19 +2,24 @@
 //  Constants.swift
 //  ExponeaSDK
 //
-//  Created by Ricardo Tokashiki on 05/04/2018.
+//  Created by Ricardo Tokashiki on 06/04/2018.
 //  Copyright © 2018 Exponea. All rights reserved.
 //
 
 import Foundation
 
-/// enum that contains network configurations constants.
+/// enum with constants used through the SDK
 enum Constants {
+    /// Network
     enum Repository {
         static let baseURL = "https://api.exponea.com/"
         static let contentType = "application/json"
         static let headerContentType = "Content-Type"
         static let headerAccept = "Accept"
         static let headerContentLenght = "Content-length"
+    }
+    /// Plist keys
+    enum Keys {
+        static let token = "exponeaProjectIdKey"
     }
 }
