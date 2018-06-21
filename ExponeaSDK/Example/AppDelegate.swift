23e6614ce3a42bec45b8de6fa5443a086af48129
Dominik Hádl <doha@nodesagency.com>
2018-06-21 12:43:16 +0200

    Fix small bugs in example app

--- ExponeaSDK/Example/AppDelegate.swift
+++ ExponeaSDK/Example/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static let memoryLogger = MemoryLogger()
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Exponea.logger = AppDelegate.memoryLogger
         Exponea.logger.logLevel = .verbose
         
--- ExponeaSDK/Example/Views/AuthenticationViewController.swift
+++ ExponeaSDK/Example/Views/AuthenticationViewController.swift
@@ -13,7 +13,7 @@ class AuthenticationViewController: UIViewController {
     
     @IBOutlet weak var tokenField: UITextField! {
         didSet {
-            tokenField.addTarget(self, action: #selector(tokenUpdated), for: UIControl.Event.editingChanged)
+            tokenField.addTarget(self, action: #selector(tokenUpdated), for: .editingChanged)
         }
     }
     @IBOutlet weak var authField: UITextField!
