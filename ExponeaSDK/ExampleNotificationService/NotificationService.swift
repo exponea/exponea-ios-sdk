4a9642374ad7fd9a48747711b28f9f1c48fef795
Dominik Hádl <doha@nodesagency.com>
2018-11-22 21:04:13 +0100

    Implement Exponea service

--- ExponeaSDK/ExampleNotificationService/NotificationService.swift
+++ ExponeaSDK/ExampleNotificationService/NotificationService.swift
@@ -7,30 +7,19 @@
 //
 
 import UserNotifications
+import ExponeaSDK_Notifications
 
 class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
+    let exponeaService = ExponeaNotificationService()
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
+        exponeaService.process(request: request, contentHandler: contentHandler)
     }
 
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    override func serviceExtensionTimeWillExpire() {
+        exponeaService.serviceExtensionTimeWillExpire()
+    }
 }
