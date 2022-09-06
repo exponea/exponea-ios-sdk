fbd397ba0cd9176cdd4fe12b3b53bb1e582c8d6f
adam1929 <103995671+adam1929@users.noreply.github.com>
2022-09-06 21:49:36 +0200

    fix/DatabaseManager init for SPM (#13)

--- ExponeaSDK/ExponeaSDK/Classes/Database/DatabaseManager.swift
+++ ExponeaSDK/ExponeaSDK/Classes/Database/DatabaseManager.swift
@@ -16,12 +16,11 @@ class DatabaseManager {
     private let context: NSManagedObjectContext
 
     internal init(persistentStoreDescriptions: [NSPersistentStoreDescription]? = nil) throws {
-//        #if SWIFT_PACKAGE
-//        let bundle = Bundle.module
-//        #else
-//
-//        #endif
+        #if SWIFT_PACKAGE
+        let bundle = Bundle.module
+        #else
         let bundle = Bundle(for: DatabaseManager.self)
+        #endif
 
         guard let container = NSPersistentContainer(name: "DatabaseModel", bundle: bundle) else {
             throw DatabaseManagerError.unableToCreatePersistentContainer
