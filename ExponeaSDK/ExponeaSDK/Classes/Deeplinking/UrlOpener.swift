42dfd8b146a81633ce1fa8450bf31379666fee6a
adam1929 <103995671+adam1929@users.noreply.github.com>
2025-05-14 14:47:49 +0200

    Uses callback implementation in URL opener

--- ExponeaSDK/ExponeaSDK/Classes/Deeplinking/UrlOpener.swift
+++ ExponeaSDK/ExponeaSDK/Classes/Deeplinking/UrlOpener.swift
@@ -23,29 +23,32 @@ final class UrlOpener: UrlOpenerType {
             Exponea.logger.log(.warning, message: "Provided url \"\(urlString)\" is invalid")
             return
         }
-        if !openUniversalLink(url, application: UIApplication.shared) {
-            openURLSchemeDeeplink(url, application: UIApplication.shared)
+        self.openUniversalLink(url, application: UIApplication.shared) { result in
+            if !result {
+                self.openURLSchemeDeeplink(url, application: UIApplication.shared)
+            }
         }
     }
 
-    private func openUniversalLink(_ url: URL, application: UIApplication) -> Bool {
+    private func openUniversalLink(_ url: URL, application: UIApplication, callBackHandler: @escaping (Bool) -> Void) {
         // Validate this is a valid URL, prevents NSUserActivity crash with invalid URL
         // only http/https is allowed
         // https://developer.apple.com/documentation/foundation/nsuseractivity/1418086-webpageurl
         // eg. MYDEEPLINK::HOME:SCREEN:1, exponea://deeplink
         guard url.absoluteString.isValidURL, url.scheme == "http" || url.scheme == "https" else {
-            return false
+            callBackHandler(false)
+            return
         }
         // Simulate universal link user activity
         let userActivity = NSUserActivity(activityType: NSUserActivityTypeBrowsingWeb)
         userActivity.webpageURL = url
 
         // Try and open the link as universal link
-        return application.delegate?.application?(
-            application,
-            continue: userActivity,
-            restorationHandler: { _ in }
-        ) ?? false
+        if application.delegate?.application?(application, continue: userActivity, restorationHandler: { _ in }) ?? false {
+            callBackHandler(true)
+        } else {
+            callBackHandler(false)
+        }
     }
 
     private func openURLSchemeDeeplink(_ url: URL, application: UIApplication) {
